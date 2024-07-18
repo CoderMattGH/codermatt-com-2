0c6ad8ec14011ccc82261988d4c7d9a0181aa94d
GANDHIX <mattdixon2000@hotmail.com>
2024-07-19 00:12:40 +0100

    Move to global styles

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -9,18 +9,18 @@ export function About() {
 
       <div className="grid-paragraph-container">
         <div>
-          <p className="about-para">
+          <p className="grid-para">
             My main passion is Java, in particular the Spring Framework.
             However, I am also skilled in web development using HTML, CSS,
             Javascript and frameworks like React.js and JQuery.
           </p>
-          <p className="about-para">
+          <p className="grid-para">
             I fully integrate the features offered by Spring into my Java
             projects by using dependency injection, allowing me to write robust
             classes that are highly extensible. This aids in keeping the
             software that I write loosely coupled with high cohesion.
           </p>
-          <p className="about-para">
+          <p className="grid-para">
             I understand the importance of testing, and I am versed in tools
             including JUnit and Mockito. I write my software with testing in
             mind by designing classes based on interfaces that can be easily
@@ -28,14 +28,14 @@ export function About() {
           </p>
         </div>
         <div>
-          <p className="about-para">
+          <p className="grid-para">
             I have experience using and administrating SQL databases including
             MySQL and H2. I am adept with using ORM frameworks including
             Hibernate and JPA in order to model database entities as objects. I
             have experience using transactional contexts in order to achieve
             atomicity, consistency, isolation, and durability (ACID).
           </p>
-          <p className="about-para">
+          <p className="grid-para">
             I am proficient in applying common software design patterns with a
             strong motivation to write well-structured and readable code which
             is easily extensible. I adhere to the principles of low coupling and
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,27 +1,30 @@
+import { Link } from "react-router-dom";
 import "./index.css";
 
 export function Header() {
   return (
     <header className="header">
       <div>
-        <img src="./coder-matt-logo.svg"></img>
+        <Link to="/">
+          <img src="./coder-matt-logo.svg"></img>
+        </Link>
       </div>
       <div>
         <nav className="nav">
           <li className="nav__li">
-            <a className="nav-link" href="#">
+            <Link className="nav-link" to="/">
               About
-            </a>
+            </Link>
           </li>
           <li className="nav__li">
-            <a className="nav-link" href="#">
+            <Link className="nav-link" to="/projects">
               Projects
-            </a>
+            </Link>
           </li>
           <li className="nav__li">
-            <a className="nav-link" href="#">
+            <Link className="nav-link" to="/">
               Links
-            </a>
+            </Link>
           </li>
         </nav>
       </div>
