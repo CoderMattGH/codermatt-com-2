53163c13e19f96ace0c15f31632045dd44dbb495
GANDHIX <mattdixon2000@hotmail.com>
2024-07-19 00:10:30 +0100

    Add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
+import ReactDom from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import { Header } from "./components/Header";
 import { About } from "./components/About";
+import { Projects } from "./components/Projects";
 
 import "./App.css";
 
 export function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
       <main className="main">
-        <About />
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="projects" element={<Projects />} />
+          {/* <Route path="*" element={} /> */}
+        </Routes>
       </main>
-    </>
+    </BrowserRouter>
   );
 }
