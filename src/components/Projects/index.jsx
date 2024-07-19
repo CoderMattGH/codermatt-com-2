825839066349ed7596ab4f88eae3aae8567a1bbe
GANDHIX <mattdixon2000@hotmail.com>
2024-07-19 01:13:05 +0100

    Add NC-News project

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -7,10 +7,10 @@ export function Projects() {
         <h2 className="project-name">Voluntier</h2>
         <p className="project-para">
           Voluntier is a mobile app that I worked on as part of a team for our
-          final project on the NorthCoders bootcamp. The app aims to connect
-          volunteers with charity organisations that need help whilst also
-          gamifying the experience by offering the user achievment badges and
-          leaderboards.
+          final project on the NorthCoders software engineering bootcamp. The
+          app aims to connect volunteers with charity organisations that need
+          help whilst also gamifying the experience by offering the user
+          achievment badges and leaderboards.
         </p>
         <p className="project-info">
           <span className="project-info__label">Tech Stack (Front-end):</span>{" "}
@@ -24,6 +24,7 @@ export function Projects() {
           <span className="project-info__label">Source Code (Front-end): </span>
           <a
             className="project-info-link"
+            target="_blank"
             href="https://github.com/CoderMattGH/vountier-fe"
           >
             https://github.com/CoderMattGH/vountier-fe
@@ -33,6 +34,7 @@ export function Projects() {
           <span className="project-info__label">Source Code (Back-end): </span>
           <a
             className="project-info-link"
+            target="_blank"
             href="https://github.com/CoderMattGH/vountier-be"
           >
             https://github.com/CoderMattGH/vountier-be
@@ -40,14 +42,67 @@ export function Projects() {
         </p>
         <div className="youtube-embed">
           <embed
+            className="youtube-embed__video"
             src="https://www.youtube.com/embed/zE38K-jLEzc?controls=1"
             wmode="transparent"
             type="video/mp4"
-            width="100%"
+            width="99%"
             height="100%"
             allow="autoplay; encrypted-media; picture-in-picture"
             title="Keyboard Cat"
           ></embed>
+        </div>
+      </article>
+
+      <article>
+        <h2 className="project-name">NC-News</h2>
+        <p className="project-para">
+          NC-News is a full-stack web application that I developed on the
+          NorthCoders software engineering bootcamp. It is a social news
+          aggregation, web content rating and discussion application similiar to
+          Reddit.
+        </p>
+        <p className="project-info">
+          <span className="project-info__label">Tech Stack (Front-end):</span>{" "}
+          React, Axios, Javascript.
+        </p>
+        <p className="project-info">
+          <span className="project-info__label">Tech Stack (Back-end):</span>{" "}
+          Javascript, Express, Jest, Node-Postgres, PostgreSQL.
+        </p>
+        <p className="project-info">
+          <span className="project-info__label">Source Code (Front-end): </span>
+          <a
+            className="project-info-link"
+            target="_blank"
+            href="https://github.com/CoderMattGH/nc-news-fe"
+          >
+            https://github.com/CoderMattGH/nc-news-fe
+          </a>
+        </p>
+        <p className="project-info">
+          <span className="project-info__label">Source Code (Back-end): </span>
+          <a
+            className="project-info-link"
+            target="_blank"
+            href="https://github.com/CoderMattGH/nc-news-be"
+          >
+            https://github.com/CoderMattGH/nc-news-be
+          </a>
+        </p>
+        <p className="project-info">
+          <span className="project-info__label">Link: </span>
+          <a
+            className="project-info-link"
+            target="_blank"
+            href="https://nc-news.codermatt.com/"
+          >
+            https://nc-news.codermatt.com/
+          </a>
+        </p>
+        <div className="project-image-container">
+          <img className="project-image" src="/images/nc-news3.png"></img>
+          <img className="project-image" src="/images/nc-news2.png"></img>
         </div>
       </article>
     </div>
