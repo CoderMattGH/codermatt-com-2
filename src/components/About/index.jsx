5e8b95eb2787762aa5cad7c7ab2fb48a85da6a1c
GANDHIX <mattdixon2000@hotmail.com>
2024-07-20 14:21:06 +0100

    Tweak spacing on about page

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -26,27 +26,27 @@ export function About() {
       </section>
       <section className="about-section">
         <h2 className="sub-heading">I possess a range of technical skills.</h2>
-        <p className="grid-para">
+        <p className="about-info">
           <span className="label">Languages: </span>Java, Javascript,
           Typescript, C, HTML, CSS.
         </p>
-        <p className="grid-para">
+        <p className="about-info">
           <span className="label">Testing: </span>Test Driven Development (TDD),
           Jest, Supertest, JUnit, Mockito.
         </p>
-        <p className="grid-para">
+        <p className="about-info">
           <span className="label">Back-end: </span>SQL, PSQL, H2, MySQL,
           Spring/Spring Boot.
         </p>
-        <p className="grid-para">
+        <p className="about-info">
           <span className="label">Front-end: </span>React, jQuery, Thymeleaf,
           Lighthouse and other accessibility tools.
         </p>
-        <p className="grid-para">
+        <p className="about-info">
           <span className="label">Hosting: </span>Cloudflare, Netlify, Apache
           HTTP Server, Apache Tomcat, Ubuntu Server administration.
         </p>
-        <p className="grid-para">
+        <p className="about-info">
           <span className="label">Soft-skills: </span>Paired programming,
           technical communication, SDL, agile and SCRUM methodologies.
         </p>
