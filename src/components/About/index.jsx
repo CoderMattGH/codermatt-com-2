3054c0f3b736313881b58f549c1935b3628c2007
GANDHIX <mattdixon2000@hotmail.com>
2024-07-22 20:21:21 +0100

    Add read-more link

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import "./index.css";
 
 export function About() {
+  const [respReadMore, setRespReadMore] = useState(false);
+
   return (
     <div className="about-container">
-      <section className="about-section">
+      <section className="about-section about-section-unresponsive">
         <h2 className="sub-heading">
           Software developer based in Manchester, UK.
         </h2>
@@ -12,18 +15,68 @@ export function About() {
           developed skills in my own free time, boot camps and through The
           University of Manchester's Software Engineering BSc (Hons) programme.
         </p>
-        <p className="grid-para">
+        <p className="grid-para grid-para-read-more">
           I began programming at a very young age using my brother's old Amstrad
           CPC 464 in BASIC. Since then, I have learnt a variety of different
           languages, techniques and developed a deeper understanding of the
           underlying architectures. My passion for creating things using
           computers has only grown over time.
         </p>
-        <p className="grid-para">
+        <p className="grid-para grid-para-read-more">
           On this website you will find projects that I have worked on, links to
           my GitHub, contact details and other interesting information about me.
         </p>
       </section>
+
+      <section className="about-section about-section-responsive">
+        <h2 className="sub-heading">
+          Software developer based in Manchester, UK.
+        </h2>
+        <p className="grid-para">
+          I am a highly-motivated computer programming enthusiast who has
+          developed skills in my own free time, boot camps and through The
+          University of Manchester's Software Engineering BSc (Hons) programme.
+        </p>
+
+        {!respReadMore ? (
+          <p className="read-more-link-p">
+            <a
+              className="read-more-link"
+              onClick={(e) => {
+                setRespReadMore(true);
+              }}
+            >
+              Read more ⇓
+            </a>
+          </p>
+        ) : (
+          <>
+            <p className="grid-para grid-para-read-more">
+              I began programming at a very young age using my brother's old
+              Amstrad CPC 464 in BASIC. Since then, I have learnt a variety of
+              different languages, techniques and developed a deeper
+              understanding of the underlying architectures. My passion for
+              creating things using computers has only grown over time.
+            </p>
+            <p className="grid-para grid-para-read-more">
+              On this website you will find projects that I have worked on,
+              links to my GitHub, contact details and other interesting
+              information about me.
+            </p>
+            <p className="read-more-link-p">
+              <a
+                className="read-more-link"
+                onClick={(e) => {
+                  setRespReadMore(false);
+                }}
+              >
+                Read less ⇑
+              </a>
+            </p>
+          </>
+        )}
+      </section>
+
       <section className="about-section">
         <h2 className="sub-heading">I possess a range of technical skills.</h2>
         <p className="about-info about-info--first">
