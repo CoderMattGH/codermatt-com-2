1c79e4ef251889a29b4c278e862ed110d0d89db9
GANDHIX <mattdixon2000@hotmail.com>
2024-07-23 18:06:48 +0100

    Fix iphone video issue

---
+++ src/components/Loading/index.jsx
@@ -0,0 +1,13 @@
+import "./index.css";
+
+export function Loading() {
+  return (
+    <div className="loading-container">
+      <img
+        className="loading-image"
+        alt="Loading..."
+        src="/images/icons/codermatt.svg"
+      />
+    </div>
+  );
+}
